952a799c920569112a6ee2b5e3691b1b5903d4fd
Patryk Kiedrowski <patryk.kiedrowski@bling.sh>
2019-11-07 11:01:52 +0100

    code refactor

--- index.js
+++ index.js
@@ -5,70 +5,88 @@ const settings = {
 		width: 20,
 		height: 80,
 		color: 'red',
-		speed: 7
+		speed: 6
 	}, 
 	ball: {
-		speed: 5
-	},
-	factors: {
-		angleRandomness: 2
+		speed: 7
 	}
 };
 
 let players = [];
 let ball;
 
-document.addEventListener('keydown', (event) => {
-	if(event.code === 'ArrowDown') {
-    players[1].direction = 1;
-	}
-	
-	if(event.code === 'ArrowUp') {
-    players[1].direction = -1;
-	}
-	
-	
-	if(event.code === 'KeyS') {
-    players[0].direction = 1;
-	}
-	
-	if(event.code === 'KeyW') {
-    players[0].direction = -1;
-  }
-}, true);
-
-document.addEventListener('keyup', (event) => {
-
-  if(event.code === 'ArrowDown' || event.code === 'ArrowUp') {
-    players[1].direction = 0;
-	}
-	
-	if(event.code === 'KeyS' || event.code === 'KeyW') {
-    players[0].direction = 0;
-  }
-});
-
 function setup() {
+	applyCanvasDimensions();
+
+	createBall();
+	createPlayers();
+	
+	startMappingPressedKeys();
+	startMappingReleasedKeys();
+
+	draw();
+}
+
+function applyCanvasDimensions() {
 	canvas.width = 1000;
 	canvas.height = 600;
-	players = [];
-  ball = new Ball();
+	// canvas.width = 0.8 * window.innerWidth;
+	// canvas.height = 0.8 * window.innerHeight;
+}
+
+function createBall() {
+	ball = new Ball();
   ball.resetBall();
-	
+}
+
+function createPlayers() {
+	players = [];
 	for(let i = 0; i < 2; i++) {
 		players.push(new Player());
   }
 	
 	players[1].x = canvas.width - 50 - players[1].width;
-	
-	draw();
+}
+
+function startMappingPressedKeys() {
+	document.addEventListener('keydown', (event) => {
+		if(event.code === 'ArrowDown') {
+			players[1].direction = 1;
+		}
+		
+		if(event.code === 'ArrowUp') {
+			players[1].direction = -1;
+		}
+		
+		
+		if(event.code === 'KeyS') {
+			players[0].direction = 1;
+		}
+		
+		if(event.code === 'KeyW') {
+			players[0].direction = -1;
+		}
+	});
+}
+
+function startMappingReleasedKeys() {
+	document.addEventListener('keyup', (event) => {
+
+		if(event.code === 'ArrowDown' || event.code === 'ArrowUp') {
+			players[1].direction = 0;
+		}
+		
+		if(event.code === 'KeyS' || event.code === 'KeyW') {
+			players[0].direction = 0;
+		}
+	});	
 }
 
 function draw() {
 	requestAnimationFrame(draw);
 	ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 	
-	for (let i = 0; i < 2; i++) {
+	for (let i = 0; i < players.length; i++) {
 		players[i].draw();
 		players[i].move();
 	}
@@ -78,23 +96,26 @@ function draw() {
   ball.collisionDetection();
 }
 
-function Player() {
-	this.x = 50;
-	this.y = canvas.height / 2;
-	this.dy = settings.player.speed;
-	this.width = settings.player.width;
-	this.height = settings.player.height;
-	this.color = settings.player.color;
-  this.direction = 0;
-  this.score = 0;
-  this.scoreElement = document.getElementById(`p${players.length}`);
+class Player {
+	constructor() {
+		this.x = 50;
+		this.y = canvas.height / 2;
+		this.dy = settings.player.speed;
+		this.width = settings.player.width;
+		this.height = settings.player.height;
+		this.color = settings.player.color;
+		this.direction = 0;
+		this.score = 0;
+		this.scoreElement = document.getElementById(`p${players.length}`);
+	}
 	
-	this.draw = function() {
+	
+	draw() {
 		ctx.fillStyle = this.color;
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 	}
 	
-  this.move = function() {
+  move() {
 		if (this.direction === 1) {
 			if (!(this.y < canvas.height - players[0].height)) {
 				return;
@@ -110,23 +131,25 @@ function Player() {
     this.y += this.dy * this.direction;
   }
   
-  this.increaseScore = function() {
+  increaseScore() {
     this.score++;
     this.scoreElement.textContent = this.score;
   }
 }
 
-function Ball() {
-	this.radius = 10;
+class Ball {
+	constructor() {
+		this.radius = 10;
+	}
 	
-	this.draw = function() {
+	draw() {
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
 		ctx.fillStyle = 'white';
 		ctx.fill();
 	}
 	
-	this.update = function() {
+	update() {
 		this.x += this.dx;
 		this.y += this.dy;
 		this.magnitude = Math.sqrt(Math.pow(this.dx, 2) + Math.pow(this.dy, 2));
@@ -147,19 +170,17 @@ function Ball() {
     }
 	}
 	
-	this.collisionDetection = function() {
+	collisionDetection() {
 		if (this.x + this.radius == players[1].x && this.y >= players[1].y && this.y <= players[1].y + settings.player.height) {
 			this.dx = -this.dx;
-      this.dy = -newBallDYBasedonOnHitPosition(this.y, players[1].y);
 		}
 		
 		if (this.x - this.radius == players[0].x + players[0].width && this.y >= players[0].y && this.y <= players[0].y + settings.player.height) {
 			this.dx = -this.dx;
-      this.dy = -newBallDYBasedonOnHitPosition(this.y, players[0].y);
 		}
   }
   
-  this.resetBall = function() {
+  resetBall() {
     const randomSignX = Math.random() > 0.5 ? 1 : -1;
     const randomSignY = Math.random() > 0.5 ? 1 : -1;
 
@@ -169,22 +190,6 @@ function Ball() {
     this.dx = settings.ball.speed * randomSignX;
     this.dy = settings.ball.speed * Math.random() * randomSignY;
   }
-}
-
-// function calculateDeflectionAngle(balldx, balldy, ballY, playerY) {
-// 	const halfPlayerHeight = settings.player.height / 2;
-// 	const hitHeightFactor = (playerY - ballY + halfPlayerHeight) / halfPlayerHeight;
-	
-// 	const atan2Angle = Math.atan2(balldy, balldx) * (180 / Math.PI);
-// 	const degrees = atan2Angle;
-// 	// console.log(degrees);
-// }
-
-function newBallDYBasedonOnHitPosition(ballY, playerY) {
-  const playerHeight = settings.player.height;
-  const hitYPositionFromCenter = (playerHeight / 2) - ballY + playerY;
-
-  return hitYPositionFromCenter / (playerHeight / 2);
 }
 
 setup();
